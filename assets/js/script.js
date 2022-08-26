7929d8f46c4567950d83d24a441359e30d56983d
MLanfear <maxjlanfear@gmail.com>
2022-08-25 18:16:52 -0700

    break time

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,42 +18,12 @@ $.ajax({
     url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
     headers: { 'X-Api-Key': '+x5KWFt82vHMdy+BPrBIbw==VMmRfElnZsH4wS9Q'},
     contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
+    success: function(results) {
+        console.log(results);
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "15098ea48cmsh2b43d6524135209p13664ejsnd412390146b4",
-		"X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com"
-	}
-};
-    console.log(settings);
-
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
 });
 
 
