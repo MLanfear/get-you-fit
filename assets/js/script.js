7f00e4b5a96dc5ff0c05efb0c1f59baaf7e9906b
MLanfear <maxjlanfear@gmail.com>
2022-08-27 21:44:26 -0700

    change of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,23 @@
-$(document).ready(function () {
-
-
-	function getRecipeAPI () {
-		var queryURL2 = "https://api.api-ninjas.com/v1/recipe?query=" + "+x5KWFt82vHMdy+BPrBIbw==VMmRfElnZsH4wS9Q";
-		$.ajax({
-			url:queryURL2,
-			method: "GET"
-		
-			}).then(function(reponse){
-				console.log(response);
-			
-			
-			})
-
-			$(".card-deck").empty();
-
-			var icon = response.recipe[0].icon;
-			var iconImg = $("<img>");
-			iconImg.addClass("img-fluid");
-			iconImg.attr("src")
-			$("title").append(iconImg);
-	}
-
-
-
-
-
-	var query = 'chicken'
-	$.ajax({
-		method: 'GET',
-		url: 'https://api.api-ninjas.com/v1/recipe?query=' + query,
-		headers: { 'X-Api-Key': '+x5KWFt82vHMdy+BPrBIbw==VMmRfElnZsH4wS9Q'},
-		contentType: 'application/json',
-		success: function(result) {
-			console.log(result);
-		},
-		error: function ajaxError(jqXHR) {
-			console.error('Error: ', jqXHR.responseText);
-		}
-	});
-
-	var muscle = 'biceps'
-	$.ajax({
-		method: 'GET',
-		url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
-		headers: { 'X-Api-Key': '+x5KWFt82vHMdy+BPrBIbw==VMmRfElnZsH4wS9Q'},
-		contentType: 'application/json',
-		success: function(results) {
-			console.log(results);
-		},
-		error: function ajaxError(jqXHR) {
-			console.error('Error: ', jqXHR.responseText);
-		}
-	});
-
-});
+const Food_chicken = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "5ed11fb8cfmsh44a3a48e404d3fbp11502cjsn56f496b4d96a",
+        "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com"
+    }
+};
+fetch("https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=chicken&query=beef&query=vegan", Food_chicken)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+    const Workout = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": "5ed11fb8cfmsh44a3a48e404d3fbp11502cjsn56f496b4d96a",
+            "X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com"
+        }
+    };
+    fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps", Workout)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 window.$ = jQuery
