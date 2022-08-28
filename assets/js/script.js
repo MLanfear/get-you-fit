9592acb60f7aa5b546be3d871f6f68be3f52cd4f
MLanfear <maxjlanfear@gmail.com>
2022-08-28 14:28:26 -0700

    undid 3 lines i made a mistake

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+<<<<<<< HEAD
+const Food_chicken = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "5ed11fb8cfmsh44a3a48e404d3fbp11502cjsn56f496b4d96a",
+        "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com"
+    }
+};
+fetch("https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=chicken&query=beef&query=vegan", Food_chicken)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+    const Workout = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": "5ed11fb8cfmsh44a3a48e404d3fbp11502cjsn56f496b4d96a",
+            "X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com"
+        }
+    };
+    fetch("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps", Workout)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+window.$ = jQuery
+=======
 const search_recipe_btn = document.getElementById("search_recipes");
 const meal_container = document.getElementById("meal")
 
@@ -53,3 +78,4 @@ fetch('https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=chicken&query
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
+>>>>>>> a617fe60209a1c8c4f62e0f1e214870321509ba4
