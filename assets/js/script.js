1dbcfd5c1591d29adbc980e16fa84f5f82c7649e
MLanfear <maxjlanfear@gmail.com>
2022-08-30 18:31:37 -0700

    oops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,61 @@
-const search_recipe_btn = document.getElementById("search_recipes");
-const meal_container = document.getElementById("meal")
+const url = ("https://api.api-ninjas.com/v1/recipe?query=");
+  headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'X-Api-Key': '15098ea48cmsh2b43d6524135209p13664ejsnd412390146b4'
+
+};
 
 
-search_recipe_btn.addEventListener('click', () => {
-	fetch('https://www.themealdb.com/api/json/v1/1/random.php') 
-	.then(res => res.json())
-	.then(res => {
-		createMeal(res.meals[0]);
-	})
-	.catch(e => {
-		console.warn(e);
-	});
-    
+const meal_container = document.getElementById("meal");
+const get_meal_btn = document.getElementById("search_recipes");
+
+if (get_meal_btn) {
+  get_meal_btn.addEventListener("click", () => {
+    console.log('btn clicked');
+  });
+}
+
+get_meal_btn.addEventListener("click", function() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      createMeal(res.meals[0]);
+    })
+    .catch(e => {
+      console.warn(e);
+    });
 });
 
-
-function createMeal(meal) {
-    const ingredients = [];
-
-    // Get all ingredients from the object. Up to 20
-    for (let i = 1; i <= 20; i++) {
-        if (meal[`strIngredient${i}`]) {
-            ingredients.push(
-                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
-            );
-        } else {
-            // Stop if there are no more ingredients
-            break;
-        }
+const createMeal = (meal) => {
+  const ingredients = [];
+  // Get all ingredients from the object. Up to 20
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      // Stop if no more ingredients
+      break;
     }
-};
-const Food_chicken = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5ed11fb8cfmsh44a3a48e404d3fbp11502cjsn56f496b4d96a',
-		'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
-	}
+  }
+
+
 };
 
-fetch('https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=chicken&query=beef&query=vegan', Food_chicken)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+function  get_meal(){
+  return url,
+  {
+    method: "GET",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-Api-Key': '15098ea48cmsh2b43d6524135209p13664ejsnd412390146b4'
+    },
+  }
+}
+meal_container().then(response => console.log(response));
 
-    const Workout = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '5ed11fb8cfmsh44a3a48e404d3fbp11502cjsn56f496b4d96a',
-            'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps', Workout)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+
+
