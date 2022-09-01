451ae30787476a3ca3fe0a605a2578b92f0d80f9
MLanfear <maxjlanfear@gmail.com>
2022-08-31 17:19:58 -0700

    updated js trying to make it show

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,118 @@
-const recipeInput = document.getElementById("meal")
-const butn = document.getElementById("search_recipes")
+const recipeInputEl = document.getElementById("meal");
+const butnEl = document.getElementById("search_recipes");
 
 
+const element = document.createElement('li');
 
 
+element.textContent = recipeInputEl
+//element.appendChild(recipeInputEl);
 
-
-
-
-
-butn.addEventListener('click', () => {
-	fetch('https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=beef', foodRecipe)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-});
-console.log("search-results")
-
+const container = document.querySelector('.container-to-put-the-items');
+//container.appendChild(element);
+//const searchFormEl = document.querySelector("#search-form");
+//const recipeInputEl = document.querySelector("#recipe-input");
+//
+//
+//var formSubmitHandler = function(event) {
+//  event.preventDefault();
+//  console.log(event);
+//}
+//
+//searchFormEl.addEventListener("submit", formSubmitHandler);
+//
+////
 var foodRecipe = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5ed11fb8cfmsh44a3a48e404d3fbp11502cjsn56f496b4d96a',
-		'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
-	}
+  method: 'GET',
+    headers: {
+    'X-RapidAPI-Key': '15098ea48cmsh2b43d6524135209p13664ejsnd412390146b4',
+    'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
+  }
 };
 
-const li = document.querySelector("div.card input[list-group-item]");
+var query = recipeInputEl.value;
+fetch('https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=' + query, foodRecipe)
+query.appendChild(foodRecipe);
+
+//
+//
+//
+//
+//
+//
+//
+//
+//var getTitleName = function() {
+//  var queryString = document.location.search;
+//  var titleName = queryString.split("=")[1];
+//  if (titleName) {
+//    repoNameEl.textContent = titleName;
+//  } else {
+//    document.location.replace('./index.html');
+//  }
+//};
+butnEl.addEventListener('click', () => {
+  fetch('https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=' + query)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+});
+
+  
+
+//fetch ('https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=title')
+//  .then(function() {
+//  })
+//  .catch(function() {
+//  });
 
 
-	
 
-        
-        
-        
-        
-var Workout = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '5ed11fb8cfmsh44a3a48e404d3fbp11502cjsn56f496b4d96a',
-      'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
-      }
-    };
-    
-    fetch('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?', Workout)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+
+
+
+// butn.querySelector(".meal").appendChild(paragraph);
+//
+//const li = document.querySelector("div.card input[list-group-item]");
+//
+//
+//  
+//
+//        
+//        
+//        
+//        
+//const Workout = {
+//    method: 'GET',
+//    headers: {
+//      'X-RapidAPI-Key': '15098ea48cmsh2b43d6524135209p13664ejsnd412390146b4',
+//      'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+//      }
+//};
+//    
+//
+//butn.addEventListener('click', function() {
+//  fetch('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?', Workout) 
+//  .then(response => response.json())
+//  .then(response => console.log(response))
+//  .catch(err => console.error(err));
+//});
+//
+//async function postData(url = '', data = {}) {
+//  const response = await fetch(url, {
+//    method: 'POST',
+//    mode: 'cors',
+//    cache: 'no-cache',
+//    credentials: 'same-origin',
+//    headers: {
+//      'Content-Type': 'application/json'
+//    },
+//    redirect: 'follow',
+//    referrerPolicy: 'no-referrer',
+//    body: JSON.stringify(data)
+//  });
+//}
+//
+
 
 
